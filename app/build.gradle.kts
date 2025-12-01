60e74388ae90b25a41f116fa86028807c0696fce
lyalin <lyalin@retail-soft.pro>
2025-12-01 09:54:49 +0300

    Update version for RuStore

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         applicationId = "com.stanislavlyalin.pomodoroapp"
         minSdk = 24
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
